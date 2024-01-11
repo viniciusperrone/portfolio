18eca3f89a63830050f294b477405c562d7ee96b
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-01-11 20:02:23 -0300

    fix: image metadata adjusted

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,9 +19,11 @@ export default class MyDocument extends Document {
         <Head>
           <meta property="og:title" content="Vinicius Perrone | Desenvolvedor Full Stack" />
           <meta property="og:description" content={description} />
-          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/portfolio-18889.appspot.com/o/Preview.svg?alt=media&token=e44f9be4-d70e-46e3-8d33-a4dff48b655d" />
-          <link rel="icon" href="/favicon.svg" />
+          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/portfolio-18889.appspot.com/o/Preview.png?alt=media&token=ea57934c-e13d-4308-9129-727f7ccadbe1" />
+          <meta property="og:image:width" content="208" />
+          <meta property="og:image:height" content="142" />
           <meta name="keywords" content="vinicius perrone, vinicius developer, vinicius desenvolvedor" />
+          <link rel="icon" href="/favicon.svg" />
           <Link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
           <Script defer src="https://pyscript.net/latest/pyscript.js"></Script>
         </Head>
