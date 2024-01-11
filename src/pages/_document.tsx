4e0e94de46a570a4ce1e6d0228841db97ea6b99e
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-01-10 21:58:52 -0300

    fix: replaced metadata local from _app file to _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,33 @@
+import type { Metadata } from "next";
+
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 import Link from 'next/link';
+
+const description = `
+  👋 Olá, meu nome é Vinicius. Sou apaixonado por 
+  tecnologias e por músicas. Desenvolvo sistemas
+  para desktop e mobile, sempre estou aberto a 
+  aprender coisas novas. Busco sempre desenvolver
+  as melhores soluções, prezando uma boa dinâmica e
+  a flexibilidade nas necessidades dos usuários.
+`;
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.svg",
+    },
+  ],
+  openGraph: {
+    type: "website",
+    title: "Vinicius Perrone | Full Stack Developer",
+    description: description,
+    images: "https://firebasestorage.googleapis.com/v0/b/portfolio-18889.appspot.com/o/Preview.png",
+  },
+};
 
 export default class MyDocument extends Document {
   render() {
