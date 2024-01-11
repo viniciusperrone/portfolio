984e127918b48dfd8ed0c08aa17b552f3f58c228
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-01-11 20:17:34 -0300

    fix: improving shared image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,9 +19,9 @@ export default class MyDocument extends Document {
         <Head>
           <meta property="og:title" content="Vinicius Perrone | Desenvolvedor Full Stack" />
           <meta property="og:description" content={description} />
-          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/portfolio-18889.appspot.com/o/Preview.png?alt=media&token=ea57934c-e13d-4308-9129-727f7ccadbe1" />
+          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/portfolio-18889.appspot.com/o/Preview.png?alt=media&token=301420b3-9c8f-4a66-b54d-619d4dafb52c" />
           <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="820" />
+          <meta property="og:image:height" content="675" />
           <meta name="keywords" content="vinicius perrone, vinicius developer, vinicius desenvolvedor" />
           <link rel="icon" href="/favicon.svg" />
           <Link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
