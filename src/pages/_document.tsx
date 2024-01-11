c199de51d273806524146942f23131c0a00b7fea
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-01-11 19:30:20 -0300

    fix: metadata adjusted

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
@@ -14,29 +12,17 @@ const description = `
   a flexibilidade nas necessidades dos usuários.
 `;
 
-export const metadata: Metadata = {
-  icons: [
-    {
-      rel: "icon",
-      url: "/favicon.svg",
-    },
-  ],
-  openGraph: {
-    type: "website",
-    title: "Vinicius Perrone | Full Stack Developer",
-    description: description,
-    images: "https://firebasestorage.googleapis.com/v0/b/portfolio-18889.appspot.com/o/Preview.png",
-  },
-};
-
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/portfolio-18889.appspot.com/o/Preview.png" />
+          <link rel="icon" href="/favicon.svg" />
+          <meta name="keywords" content="vinicius perrone, vinicius developer, vinicius desenvolvedor" />
           <Link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
           <Script defer src="https://pyscript.net/latest/pyscript.js"></Script>
-          <Link key="favicon" rel="icon" href="/favicon.svg" type="image/svg+xml" />
         </Head>
         <body>
           <Main />
