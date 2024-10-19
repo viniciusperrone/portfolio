37c0290622b82b2d25e385924dfa28e97de92b48
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-10-19 20:01:53 -0300

    fix: updated number in favbutton

--- src/components/FavButton.tsx
+++ src/components/FavButton.tsx
@@ -1,9 +1,9 @@
-import { BsWhatsapp } from 'react-icons/bs';
+import { BsWhatsapp } from "react-icons/bs";
 
 export function FavButton() {
   const handleClick = () => {
-    const phoneNumber = '+5592993041762';
-    const message = encodeURIComponent('Olá, podemos conversar?');
+    const phoneNumber = "+5541974017213";
+    const message = encodeURIComponent("Olá, podemos conversar?");
     const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
 
     window.open(whatsappUrl, "_blank");
@@ -14,10 +14,7 @@ export function FavButton() {
       className="shadow-gradient fixed bottom-4 right-4 lg:right-8 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-transform transform translate-y-0 hover:translate-y-px bounce-animation"
       onClick={handleClick}
     >
-      <BsWhatsapp 
-        color="white"
-        size={30}
-      />
+      <BsWhatsapp color="white" size={30} />
     </button>
   );
-};
+}
